28984b1c662036918adc4bf9264291e49c462ab5
Helga test <www.helga@mail.ru>
2023-04-17 11:09:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int miles = 20;
+
+        int bonus = amount / miles;
+
+        System.out.println("Начисленные мили: " + bonus);
+    }
+}
